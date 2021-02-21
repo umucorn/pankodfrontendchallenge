8d338152ad0b8894861712a1b5fa219a29908c4f
Umut Özdemir <umutozdemir997@gmail.com>
2021-02-21 11:51:14 +0300

    remove unused icons and add used ones

--- src/elements/icons/index.tsx
+++ src/elements/icons/index.tsx
@@ -1,9 +1,1 @@
-export { default as GithubIcon } from "./GithubIcon";
-export { default as LinkedinIcon } from "./LinkedinIcon";
-export { default as NextjsIcon } from "./NextjsIcon";
-export { default as PankodIcon } from "./PankodIcon";
 export { default as MenuIcon } from "./MenuIcon";
-export { default as TwitterIcon } from "./TwitterIcon";
-export { default as YoutubeIcon } from "./YoutubeIcon";
-export { default as SunIcon } from "./SunIcon";
-export { default as MoonIcon } from "./MoonIcon";
