327c19edf48189d561aa67d271a2c9c428c31cb2
Umut Özdemir <umutozdemir997@gmail.com>
2021-02-22 22:41:18 +0300

    remove unused components from superplate boilerplate

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { Header } from "./header";
-export { Main } from "./main";
 export { ClapperBoardCard } from "./clapperBoardCard";
 export { ImageCard } from "./imageCard";
 export { Cards } from "./cards";
